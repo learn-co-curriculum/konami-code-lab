7ac1ab1c20855236bc42457e118e2cf36354efc0
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-02-20 12:08:34 -0500

    done

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.body;
+  let index = 0;
+  function callback(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        window.alert('Congrats!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  body.addEventListener("keydown", callback);
 }
